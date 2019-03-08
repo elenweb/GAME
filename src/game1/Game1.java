154c4bc9da29404b6943e3e1f34e42251908362b
elenweb <elenweb@narod.ru>
2019-03-08 21:11:18 +0300

    Животные

--- src/game1/Game1.java
+++ src/game1/Game1.java
@@ -2,19 +2,23 @@ package game1;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 
 public class Game1 {
     static Animal first;
 
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        boolean flag = true;
         first = new Animal();
         first.name = "собака";
+
+
+
         Animal last = first;
         System.out.println("Мое любимое животное " + first.name);
         System.out.println("Введите названия ваших любимых животных по одному, когда закончите введите \"1\":");
-        while (flag) {
+        while (true) {
             try {
                 String animalName;
                 animalName = reader.readLine();
